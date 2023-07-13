7a2edd83c4f270ccf10cd76462a763065421fede
bkleinb78 <y2bk728@aol.com>
2023-07-13 12:30:00 -0500

    finished editing out all the buttons

--- pages/api/components/Button.js
+++ pages/api/components/Button.js
@@ -10,14 +10,15 @@ const Button = (props) => {
     textColor,
     leftIcon,
     rightIcon,
+    clear,
     sm,
     md,
     lg,
     xlg,
     xxlg,
+    buttonSize,
     ...rest
   } = props;
-  console.log({ props });
 
   return (
     <button
@@ -25,27 +26,26 @@ const Button = (props) => {
       disabled={props.disabled}
       className={`
       border 
-      border-${color ?? "blue"}-500  
-       bg-${color ? color : "blue"}-500 
-      // ${color ? color : "bg-blue-500"} 
-      text-${textColor ?? "white"}-900 
-      py-2.5 px-4 text-sm rounded-[5px] 
+      border-${color ? color : "blue"}-500  
+      bg-${color ? color : "blue-500"}
+      text-${textColor ?? "white"}-600 
+      rounded-[5px] 
        focus:outline-none 
        focus:ring-4 
-       ring-${color ?? "blue"}-300
+       ring-${color ? color : "blue"}-300
        items-center 
        ${
          props.disabled
            ? "opacity-50 cursor-not-allowed pointer-events-none"
-           : `hover:bg-${color ?? "blue"}-700`
+           : `hover:bg-${color ? color : "blue"}-700`
        } 
-
+       hover:bg-${color ? color : "blue"}-700
        ${sm ? "py-2 px-3.5 text-sm" : ""}
        ${md ? "py-2.5 px-4 text-sm" : ""}
        ${lg ? "py-2.5 px-[18px] text-base" : ""}
-       ${xlg ? "py-3 px-5 text-base" : ""}
+       ${xlg ? "py-3 px-5 texsmt-base" : ""}
        ${xxlg ? "py-4 px-7 text-lg" : ""}
-
+       ${clear ? "border-0 focus::ring-0" : ""}
        ${props.className ?? ""}`}
     >
       {leftIcon ? (
--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,21 @@ import Text from "./api/components/Text";
 import Button from "./api/components/Button";
 import {
   faCalendar,
+  faDotCircle,
   faPlus,
   faSearch,
 } from "@fortawesome/free-solid-svg-icons";
 
 import { Inter } from "next/font/google";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const buttonStates = ["Default", "Hover", "Focus", "Disabled"];
 
 const SubButtonCardGroup = (props) => {
-  const { color, textColor, leftIcon, rightIcon, value } = props;
+  const { color, textColor, leftIcon, rightIcon, value, clear, className } =
+    props;
   return (
     <div className=" border-solid border-blue-500 border rounded-md text-center w-max">
       <div className="grid grid-cols-5">
@@ -36,6 +39,8 @@ const SubButtonCardGroup = (props) => {
                 color={color}
                 textColor={textColor}
                 disabled={disabled}
+                clear={clear}
+                className={className}
               >
                 {value}
               </Button>
@@ -48,6 +53,8 @@ const SubButtonCardGroup = (props) => {
                 color={color}
                 textColor={textColor}
                 disabled={disabled}
+                clear={clear}
+                className={className}
               >
                 {value}
               </Button>
@@ -60,6 +67,8 @@ const SubButtonCardGroup = (props) => {
                 color={color}
                 textColor={textColor}
                 disabled={disabled}
+                clear={clear}
+                className={className}
               >
                 {value}
               </Button>
@@ -72,6 +81,8 @@ const SubButtonCardGroup = (props) => {
                 color={color}
                 textColor={textColor}
                 disabled={disabled}
+                clear={clear}
+                className={className}
               >
                 {value}
               </Button>
@@ -84,6 +95,8 @@ const SubButtonCardGroup = (props) => {
                 color={color}
                 textColor={textColor}
                 disabled={disabled}
+                clear={clear}
+                className={className}
               >
                 {value}
               </Button>
@@ -101,35 +114,109 @@ const ButtonCardGroup = (props) => {
     <div className="bg-white border-dashed border p-3 border-blue-500 flex w-max">
       <div>
         <div className="flex h-[30px]"></div>
-        <div className="flex h-[70px]">
+        <div className="flex h-[61px]">
           <Text className="py-2 px-4 my-auto">Default</Text>
         </div>
-        <div className="flex h-[70px]">
+        <div className="flex h-[61px]">
           <Text className="py-2 px-4 my-auto">Hover</Text>
         </div>
-        <div className="flex h-[70px]">
+        <div className="flex h-[61px]">
           <Text className="py-2 px-4 my-auto">Focus</Text>
         </div>
-        <div className="flex h-[70px]">
+        <div className="flex h-[61px]">
           <Text className="py-2 px-4 my-auto">Disabled</Text>
         </div>
       </div>
-      <SubButtonCardGroup value="Button CTA" />
-      <SubButtonCardGroup value="Button CTA" leftIcon={faPlus} />
-      <SubButtonCardGroup value="Button CTA" rightIcon={faPlus} />
-      <SubButtonCardGroup rightIcon={faPlus} />
+      <SubButtonCardGroup {...props} value="Button CTA" />
+      <SubButtonCardGroup {...props} value="Button CTA" leftIcon={faPlus} />
+      <SubButtonCardGroup {...props} value="Button CTA" rightIcon={faPlus} />
+      <SubButtonCardGroup {...props} rightIcon={faPlus} />
     </div>
   );
 };
 
 export default function Home() {
   return (
-    <div className="p-2 bg-red-500">
-      <ButtonCardGroup />
-      {/* <ButtonCardGroup leftIcon={faPlus} />
-        <ButtonCardGroup color={"white"} leftIcon={faPlus} textColor={"gray"} />
-        <ButtonCardGroup color={"white"} textColor={"gray"} />
-        <ButtonCardGroup color={"red"} /> */}
-    </div>
+    <>
+      <div className="p-2 bg-red-500">
+        <ButtonCardGroup />
+        <ButtonCardGroup
+          className="bg-[#306cfe26] hover:bg-[#306cfe38]"
+          color="blue-200"
+          textColor={"blue"}
+          clear
+        />
+        <ButtonCardGroup className="bg-white hover:bg-white focus:bg-white drop-shadow-sm  border-gray-300 text-gray-800 " />
+        <ButtonCardGroup
+          clear
+          className="bg-white hover:bg-[#306CFE26] focus:bg-[#306CFE38] drop-shadow-sm text-blue-800 "
+        />
+        <ButtonCardGroup
+          clear
+          className="bg-white hover:bg-gray-100 focus:bg-gray-200 text-gray-800 "
+        />
+
+        <ButtonCardGroup
+          clear
+          className="bg-white hover:bg-white hover:underline focus:bg-white text-blue-800 "
+        />
+
+        <ButtonCardGroup
+          clear
+          className="bg-white hover:bg-white hover:underline focus:bg-white text-gray-800 "
+        />
+
+        <ButtonCardGroup
+          clear
+          className="bg-red-500 hover:bg-red-600 ring-red-300 focus:bg-red-500 text-white"
+        />
+
+        <ButtonCardGroup
+          clear
+          className="bg-red-100 hover:bg-red-200 ring-red-50 focus:bg-red-100 text-red-500"
+        />
+
+        <ButtonCardGroup className="bg-white hover:bg-red-300 ring-red-50 focus:bg-white border-red-300 drop-shadow-sm text-red-500" />
+
+        <ButtonCardGroup
+          clear
+          className="bg-white hover:bg-red-100 ring-red-50 focus:bg-red-200 border-red-300  text-red-700"
+        />
+
+        <ButtonCardGroup
+          clear
+          className="bg-white hover:bg-white hover:underline ring-red-50 focus:bg-white  border-red-300  text-red-600"
+        />
+
+        <ButtonCardGroup
+          clear
+          className="bg-white hover:bg-white  ring-red-50 ring-0 focus:underline focus:bg-white  border-red-300  text-red-600"
+        />
+
+        {/* <ButtonCardGroup
+          className=" hover:bg-[#306cfe26] focus:bg-[#306cfe38]"
+          color={"white"}
+          textColor={"blue"}
+          clear={true}
+        />
+        <ButtonCardGroup color={"white"} textColor={"blue"} clear={true} />
+        <ButtonCardGroup color={"red"} />
+        <ButtonCardGroup
+          color={"white"}
+          textColor={"red"}
+          className="hover:bg-red-300 text-red-900 border-red-500"
+        />
+        <ButtonCardGroup
+          color={"white"}
+          textcolor={"red"}
+          className="text-red-"
+        />
+        <ButtonCardGroup
+          color={"white"}
+          textcolor={"red"}
+          className="hover:bg-red-400 text-red-500 border-red-300"
+        /> */}
+      </div>
+    </>
   );
 }
